9f31f8002e1a23624e5bc4c008124c0db61eef6f
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-06-07 19:58:28 +0900

    First airbone of Form

---
+++ src/components/Form.jsx
@@ -0,0 +1,19 @@
+const Form = () => {
+    return (
+        <form>
+            <input type="text" class="todo-input" />
+            <button class="todo-button" type="submit">
+                <i class="fas fa-plus-square"></i>
+            </button>
+            <div class="select">
+                <select name="todos" class="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="incompleted">Incompleted</option>
+                </select>
+            </div>
+        </form>    
+    );
+}
+
+export default Form;
