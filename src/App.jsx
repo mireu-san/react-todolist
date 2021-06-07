18d07a014776eee5da670e7973b2b742133a90c9
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-06-07 21:07:01 +0900

    #4 array

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import ToDoList from "./components/ToDoList";
 
 function App() {
   const [ inputText, setInputText ] = useState("");
+  const [ toDos, setToDos ] = useState([]);
   return (
     <div className="App">
       <header>
-        <h1>Today's Matter</h1>
+        <h1>Today's Matter {inputText}</h1>
       </header>
         <Form setInputText={setInputText}/>
         <ToDoList />
