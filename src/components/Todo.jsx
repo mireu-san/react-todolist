410fcfd73c7a850bf5c739531e01f09558fbe25a
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-06-08 22:53:27 +0900

    True - completeHandler, component, react extension, browser

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,12 +3,23 @@ import React from 'react'
 const Todo = ({ text, todo, toDos, setToDos }) => {
     // Events
     const deleteHandler = () => {
-       setToDos(toDos.filter(el => el.id !== todo.id)); // el = element
+       setToDos(toDos.filter(el => el.id !== todo.id)); // el = element / by matching the ID that's been created previsouly (existing one), it can exactly delete the one it desire.
+    };
+    const completeHandler = () => {
+        setToDos(toDos.map(item => {
+            if(item.id === todo.id){
+                return {
+                    ...item, completed: !item.completed //pass
+                }
+            }
+            return item;
+        })
+        );
     };
     return (
         <div className="todo">
             <li className="todo-item">{text}</li>
-            <button className="complete-btn">
+            <button onClick={completeHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
             <button onClick={deleteHandler} className="trash-btn">
