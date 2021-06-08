10bb313476917dbf700b336245b8fbe6d57e71d3
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-06-08 21:18:47 +0900

    correct - UI OK

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-
+// Import components
 import Todo from "./Todo";
 
 const ToDoList = () => {
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
-                
+            <ul className="todo-list">
+                <Todo />
+            </ul>
         </div>
     );
 };
