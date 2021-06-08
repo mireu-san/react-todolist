9cad2652e57f2acf7209ccfa46f91b4efaa249ed
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-06-08 21:21:34 +0900

    add the list 1/2

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,11 +2,13 @@ import React from "react";
 // Import components
 import Todo from "./Todo";
 
-const ToDoList = () => {
+const ToDoList = ({ toDos }) => {
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                <Todo />
+                {toDos.map(todo => (
+                    <Todo />
+                ))}
             </ul>
         </div>
     );
