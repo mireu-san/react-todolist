0826db539426eef15fbb51b2d607ad7f44c0f943
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-06-08 16:29:11 +0900

    created Todo.jsx

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import Todo from "./Todo";
+
 const ToDoList = () => {
     return (
         <div className="todo-container">
             <ul className="todo-list"></ul>
+                <Todo />
         </div>
     );
 };
